d3b68567f527064ac5e4b20ad90ff24f8577b50e
Ryan Chatterton <ryan.chatterton@sharpnotions.com>
2020-11-08 16:28:03 -0500

    fix: build updates

--- src/builder/build.ts
+++ src/builder/build.ts
@@ -3,9 +3,9 @@ import path from 'path';
 import fs from 'fs-extra';
 import { LATEST_SUPPORTED_EMOJI_VERSION } from './constants';
 
-const DATA_FOLDER = path.resolve(__dirname, '../../data');
+const DATA_FOLDER = path.resolve(__dirname, '../data');
 const FILENAME = `${LATEST_SUPPORTED_EMOJI_VERSION}/emoji-list-data.json`;
-const MIN_FILENAME = `${LATEST_SUPPORTED_EMOJI_VERSION}/emoji-list-data-min.json`;
+const MIN_FILENAME = `${LATEST_SUPPORTED_EMOJI_VERSION}/emoji-list-data.min.json`;
 
 const filePath = path.resolve(DATA_FOLDER, FILENAME);
 const minFilePath = path.resolve(DATA_FOLDER, MIN_FILENAME);
